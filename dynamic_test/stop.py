ec85d941699f02dee57f7ab962aa2dc5cf9037b2
Qiu Yuqing <qyuqing@umich.edu>
2021-07-15 10:38:55 +0800

    delete readme and stop.py

--- dynamic_test/stop.py
+++
@@ -1,6 +0,0 @@
-from __future__ import print_function
-from clipper_admin import ClipperConnection, DockerContainerManager
-
-
-clipper_conn = ClipperConnection(DockerContainerManager(use_centralized_log=False))
-clipper_conn.stop_all()
